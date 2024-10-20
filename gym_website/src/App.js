d236fb45c4f78ec1dbaabde0c1faa82932800d6f
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-10-20 14:29:07 +0530

    3. Navbar Component

--- gym_website/src/App.js
+++ gym_website/src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavigationBar from "./components/Navigation/NavigationBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
 }
 
 export default App;
