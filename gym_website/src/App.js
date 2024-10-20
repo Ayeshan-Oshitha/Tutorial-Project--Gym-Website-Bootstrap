1694d8d1c0789572492a420d99adfe55602e5840
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-10-20 18:38:48 +0530

    4. Hero Section

--- gym_website/src/App.js
+++ gym_website/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavigationBar from "./components/Navigation/NavigationBar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Hero />
     </div>
   );
 }
